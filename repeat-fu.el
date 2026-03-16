3da857fa31b12cff8b59d5d75f652ec733b0cbf3
Campbell Barton <ideasman42@gmail.com>
2026-03-16 18:45:46 +1100

    Cleanup: format update for newer emacs

--- repeat-fu.el
+++ repeat-fu.el
@@ -28,13 +28,11 @@
     (defmacro incf (place &optional delta)
       "Increment PLACE by DELTA or 1."
       (declare (debug (gv-place &optional form)))
-      (gv-letplace (getter setter) place
-        (funcall setter `(+ ,getter ,(or delta 1)))))
+      (gv-letplace (getter setter) place (funcall setter `(+ ,getter ,(or delta 1)))))
     (defmacro decf (place &optional delta)
       "Decrement PLACE by DELTA or 1."
       (declare (debug (gv-place &optional form)))
-      (gv-letplace (getter setter) place
-        (funcall setter `(- ,getter ,(or delta 1)))))))
+      (gv-letplace (getter setter) place (funcall setter `(- ,getter ,(or delta 1)))))))
 
 
 ;; ---------------------------------------------------------------------------
